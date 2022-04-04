403172a5147a12c8d641ea17ebd99bf8fb3bd3d4
daniyal khan <daniyal.khan@afiniti.com>
2022-04-04 13:16:32 +0500

    debug value for wind stroke added

--- src/Experience/WindStrokes.js
+++ src/Experience/WindStrokes.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import gsap from 'gsap';
+
 import Experience from './Experience.js';
 import windStrokeVertex from './shaders/windStroke/vertex.glsl';
 import windStrokeFragment from './shaders/windStroke/fragment.glsl';
@@ -8,20 +9,19 @@ export default class WindStrokes {
   constructor(_options) {
     // Options
     this.experience = new Experience();
-    this.debug = this.experience.debug;
     this.resources = this.experience.resources;
+    this.debug = this.experience.debug;
     this.scene = this.experience.scene;
 
-    this.resource1 = this.resources.items.windStroke1eModel;
+    this.resource1 = this.resources.items.windStroke1Model;
     this.resource2 = this.resources.items.windStroke2Model;
-    this.items = [];
 
     // Debug
     if (this.debug) {
       this.debugFolder = this.debug.addFolder({
         title: 'windStrokes'
-        // expanded: false
       });
+
       this.debugFolder
         .addButton({
           title: 'pop()'
@@ -33,31 +33,43 @@ export default class WindStrokes {
 
     this.setMaterial();
 
-    window.setInterval(() => {
+    const autoPop = () => {
       if (Math.random() < 0.3) {
         this.pop();
       }
-    }, 1000);
+
+      gsap.delayedCall(0.5, autoPop);
+    };
+
+    autoPop();
   }
 
   setMaterial() {
-    // Material
     this.material = new THREE.ShaderMaterial({
       transparent: true,
       depthWrite: false,
       uniforms: {
-        uProgress: { value: 0.1 }
+        uProgress: { value: 0 }
       },
       vertexShader: windStrokeVertex,
       fragmentShader: windStrokeFragment
     });
 
-    // Mesh
-    // this.reference.mesh = this.resources.scene.children[0];
-    // this.reference.mesh.position.set(0, 2, 4);
-    // this.reference.mesh.material = this.reference.material;
+    if (this.debug) {
+      this.debugFolder.addInput(this.material.uniforms.uProgress, 'value', {
+        min: 0,
+        max: 1
+      });
 
-    // this.scene.add(this.reference.mesh);
+      const mesh = this.resource1.scene.children[0].clone(false);
+      mesh.material = this.material;
+
+      mesh.position.x = 0;
+      mesh.position.y = 2;
+      mesh.position.z = 4;
+
+      this.scene.add(mesh);
+    }
   }
 
   pop() {
@@ -69,18 +81,18 @@ export default class WindStrokes {
     mesh.position.x = (3 + Math.random() * 7) * (Math.random() < 0.5 ? 1 : -1);
     mesh.position.y = Math.random() * 3;
     mesh.position.z =
-      (2.5 + Math.random() * 5) * (Math.random() < 0.5 ? 1 : -1);
+      (2.2 + Math.random() * 5) * (Math.random() < 0.5 ? 1 : -1);
 
-    // Animate with GSAP
     gsap.to(mesh.material.uniforms.uProgress, {
-      ease: 'power4.inOut',
-      value: 1,
-      duration: 12
+      ease: 'power2.inOut',
+      duration: 10,
+      value: 1
     });
 
     gsap.to(mesh.position, {
-      duration: 20,
-      x: mesh.position.x - 3.0,
+      duration: 10,
+      ease: 'power2.inOut',
+      x: mesh.position.x + 1.5,
       onComplete: () => {
         mesh.geometry.dispose();
         mesh.material.dispose();
